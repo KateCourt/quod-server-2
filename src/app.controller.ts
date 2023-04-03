5ba030e604db3ab69f0ccd5fd5062fbc2af44645
KateCourt <49239607+KateCourt@users.noreply.github.com>
2023-04-03 15:46:54 +0100

    update packages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Request, Post, Param, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { UsersService } from './users/users.service'
+import { DonorsService } from './donors/donors.service'
+import { OrgansService } from './organs/organs.service'
+import { RegionsService } from './regions/regions.service';
+import { SamplesService } from './samples/samples.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private authService: AuthService, 
+    private usersService: UsersService,
+    private donorsService: DonorsService,
+    private organsService: OrgansService,
+    private regionsService: RegionsService,
+    private samplesService: SamplesService
+    ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    
+    
+    return this.authService.login(req.user);
   }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Post('/variedBasket')
+    async variedBasket(@Request() req) {
+        
+        
+        const basketList = {
+            sampleList: await this.samplesService.findMulti(req.body.basket.samples),
+            regionList: await this.regionsService.findMulti(req.body.basket.regions),
+            organList: await this.organsService.findMulti(req.body.basket.organs),
+        }
+        
+        
+        return basketList
+    }
+
 }
